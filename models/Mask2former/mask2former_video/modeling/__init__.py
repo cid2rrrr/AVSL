ed0a4c2e15cb311fb3f3e79cbe7ccc8f6304a3df
DoSung Lee <dslee1219@korea.ac.kr>
2024-01-30 15:17:45 +0900

    Add files via upload

--- models/Mask2former/mask2former_video/modeling/__init__.py
+++ models/Mask2former/mask2former_video/modeling/__init__.py
@@ -1,2 +1,2 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
-from .transformer_decoder.video_mask2former_transformer_decoder import VideoMultiScaleMaskedTransformerDecoder
+from .video_mask2former_transformer_decoder import VideoMultiScaleMaskedTransformerDecoder
